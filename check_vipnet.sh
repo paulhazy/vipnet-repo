b9602b0dd174ec80f6dff8fe66d603052048946d
paul_hayz <35250024+paulhazy@users.noreply.github.com>
2020-01-10 19:32:49 +0400

    Update check_vipnet.sh

--- check_vipnet.sh
+++ check_vipnet.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 mftp_state=`sudo /sbin/failover info | grep "mftp state" | awk '{print $4}' `
 total=`sudo /sbin/failover info | grep "total cpu" | awk '{print $4}' `
