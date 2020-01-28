426af93b6a42057c5a5425a567bc99ac17a9ca06
paul_hazy <pavelpv8@gmail.com>
2020-01-28 11:43:08 +0300

    update

--- check_vipnet.sh
+++ check_vipnet.sh
@@ -3,17 +3,24 @@
 mftp_state=`sudo /sbin/failover info | grep "mftp state" | awk '{print $4}' `
 total=`sudo /sbin/failover info | grep "total cpu" | awk '{print $4}' `
 iplir_state=`sudo /sbin/failover info | grep "iplir state" | awk '{print $4}' `
+
+####### checking mftp state ##########
+
 if [ $mftp_state = "works" ]
 	then 
 	echo "MFTP works"
 	else
 	echo "MFTP not running"
 fi
+
+######## checking iplir state ###########
+
 if [ $iplir_state = "works" ]
 	then 
 	echo "iplir works"
 	else 
 	echo "iplir not running"
 fi
+
 echo "total cpu is $total"
 
